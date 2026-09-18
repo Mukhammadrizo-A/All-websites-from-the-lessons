91d2225754ccf386aba485151221d8f7a89f9b99
Mukhammadrizo <mukhammadrizo.abdumutalibov@gmail.com>
2026-09-18 18:58:56 +0500

    Comment out showTime function and its invocation

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-function showTime() {
-    let upload_time = document.getElementById('time');
+// function showTime() {
+//     let upload_time = document.getElementById('time');
 
-    let get_time = new Date();
+//     let get_time = new Date();
 
-    let year = get_time.getFullYear();
-    let month = get_time.getMonth() + 1;
-    let day = get_time.getDate();
+//     let year = get_time.getFullYear();
+//     let month = get_time.getMonth() + 1;
+//     let day = get_time.getDate();
 
-    if (day < 10) {
-        day = '0' + day;
-    }
-    if (month < 10) {
-        month = '0' + month;
-    }
+//     if (day < 10) {
+//         day = '0' + day;
+//     }
+//     if (month < 10) {
+//         month = '0' + month;
+//     }
 
-    upload_time.innerHTML = day + '.' + month + '.' + year;
+//     upload_time.innerHTML = day + '.' + month + '.' + year;
 
-    upload_time.style.color = 'white'
-}
+//     upload_time.style.color = 'white'
+// }
 
-window.onload = function() {
-    showTime();
+// window.onload = function() {
+//     showTime();
 
-    setInterval(showTime, 1000);
-}
+//     setInterval(showTime, 1000);
+// }
